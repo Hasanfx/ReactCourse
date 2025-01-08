8dac1211acd25646cbe056446b5a7488107395e9
Hasanfx <imrshadow.fx@gmail.com>
2025-01-08 19:38:01 +0300

    Lists & Props

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import Navbar from './navbar';
 import Home from './Home';
 
 function App() {
-  const title='Welcome to the new blog'
-  const likes=50;
   return (
     <div className="App">
       <Navbar />
     <div className="content">
       <Home/>
-      <h1>{title}</h1>
-      <p>Liked {likes} times </p>
     </div>
     </div>
   
--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,64 @@
 import { useState } from "react";
-
+import BlogList from "./blogList";
 
 const Home = () => {
-    // let name = 'mario';
-    const [name,setName]= useState('mario');
-    const [age,setAge]= useState('25');
+
+
+    const [blogs,setBlogs]=useState ([
+        {
+            id:'1',
+            title:'my new website',
+            body:'lorem Ipsum...',
+            author:'mario'
+        },
+        {
+            id:'2',
+            title:'my new website',
+            body:'lorem Ipsum...',
+            author:'luigi'
+        },   
+        {
+            id:'3',
+            title:'my new website',
+            body:'lorem Ipsum...',
+            author:'yoshi'
+        }
+    ]);
+
+    const handleDelete = (id) => {
+        const newBlogs= blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs);
+       
+     }
+    // // let name = 'mario';
+    // const [name,setName]= useState('mario');
+    // const [age,setAge]= useState('25');
    
-    const handleClick=() => {
-        setName('luigi');
-        setAge(30);
-    }
-    const handleClickAgain=(name,e) => {
-        console.log('hello ' + name ,e.target)
-    }
+    // const handleClick=() => {
+    //     setName('luigi');
+    //     setAge(30);
+    // }
+    // const handleClickAgain=(name,e) => {
+    //     console.log('hello ' + name ,e.target)
+    // }
 
     return (
-        <div className="Home">
-            <h1>Home</h1>
-            <button onClick={handleClick}>Click </button>
-            <br/>
-            <p>{name} is {age} years old </p>
-            <button onClick={(e) => {handleClickAgain('mario',e)}}>ClickAgain </button>
+        <div className="home">
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+        
+
+
         </div>
-      );
+    );
+
+        // <div className="Home">
+        //   <h1>Home</h1>
+//          {/* <button onClick={handleClick}>Click </button> */}
+//          {/* <br/> */}
+//          {/* <p>{name} is {age} years old </p> */}
+//          {/* <button onClick={(e) => {handleClickAgain('mario',e)}}>ClickAgain </button> */}
+//          </div>
+     
 }
  
 export default Home;
