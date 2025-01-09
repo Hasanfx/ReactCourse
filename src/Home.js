f2a36a353b7c23fe0f9af15fe070e6f243207df5
Hasanfx <imrshadow.fx@gmail.com>
2025-01-09 19:21:53 +0300

    useEffect & Json Server & data fetch & handling errors & Custom Hooks

--- src/Home.js
+++ src/Home.js
@@ -1,51 +1,17 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import BlogList from "./blogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
 
+    const {data:blogs,isPending,error}= useFetch('http://localhost:8000/blogs')
 
-    const [blogs,setBlogs]=useState ([
-        {
-            id:'1',
-            title:'my new website',
-            body:'lorem Ipsum...',
-            author:'mario'
-        },
-        {
-            id:'2',
-            title:'my new website',
-            body:'lorem Ipsum...',
-            author:'luigi'
-        },   
-        {
-            id:'3',
-            title:'my new website',
-            body:'lorem Ipsum...',
-            author:'yoshi'
-        }
-    ]);
-
-    const handleDelete = (id) => {
-        const newBlogs= blogs.filter(blog => blog.id !== id)
-        setBlogs(newBlogs);
-       
-     }
-    // // let name = 'mario';
-    // const [name,setName]= useState('mario');
-    // const [age,setAge]= useState('25');
-   
-    // const handleClick=() => {
-    //     setName('luigi');
-    //     setAge(30);
-    // }
-    // const handleClickAgain=(name,e) => {
-    //     console.log('hello ' + name ,e.target)
-    // }
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-        
+            {error && <div> {error} </div>}
+        {isPending && <div>Loading...</div>}
+        {blogs &&  <BlogList blogs={blogs} title="All Blogs"/>}
 
 
         </div>
@@ -58,6 +24,18 @@ const Home = () => {
 //          {/* <p>{name} is {age} years old </p> */}
 //          {/* <button onClick={(e) => {handleClickAgain('mario',e)}}>ClickAgain </button> */}
 //          </div>
+    // // let name = 'mario';
+    // const [name,setName]= useState('mario');
+    // const [age,setAge]= useState('25');
+   
+    // const handleClick=() => {
+    //     setName('luigi');
+    //     setAge(30);
+    // }
+    // const handleClickAgain=(name,e) => {
+    //     console.log('hello ' + name ,e.target)
+    // }
+
      
 }
  
